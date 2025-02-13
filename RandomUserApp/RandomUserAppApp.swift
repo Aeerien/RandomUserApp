f05ed7dfd423018b53dc4e128c4a6f62f4e99e6f
Irina Arkhireeva <aeerien@icloud.com>
2025-02-13 12:00:21 +0300

    Initial Commit

---
+++ RandomUserApp/RandomUserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUserAppApp.swift
+//  RandomUserApp
+//
+//  Created by Irina Arkhireeva on 13.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct RandomUserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
